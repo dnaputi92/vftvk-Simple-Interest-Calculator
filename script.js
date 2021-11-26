1c965a97cdaa5ca3ac548da165faf0aca20f86c9
derrick <d.naputi92@gmail.com>
2021-11-26 20:23:17 +0000

    commit on master

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if (principal<=0){  // Validation principal inputbox
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    // Show results in span
+    document.getElementById("result").innerHTML="<p>if you deposite <mark> " + principal + "</mark>,</p>" + "<p>at an interest rate of <mark>" + rate + "</mark>,</p>" + "<p>You will receive an amount of <mark>" +  interest + "</mark>,</p>" + "<p>in the year <mark>" + year + "</mark></p>";
     
 }
-        
+     
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
